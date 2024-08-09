ec4308330a3b52ddcb8ac8115bdae363097d8837
Meet shah <shahmeet644@gmail.com>
2024-08-09 10:02:12 +0530

    Update

--- lib/vegetableinfo.dart
+++ lib/vegetableinfo.dart
@@ -97,7 +97,7 @@ class _VegeInfoState extends State<VegetableInfoState> {
                         'Lettuce is an annual plant of the daisy family, Asteraceae. It is most often grown as a leaf vegetable, but sometimes for its stem and seeds. Lettuce is most often used for salads, although it is also seen in other kinds of food, such as soups, sandwiches and wraps; it can also be grilled.',
                         style: TextStyle(fontSize: 18),
                       ),
-                      SizedBox(height: 130),
+                      SizedBox(height: 80),
                       Row(
                         children: [
                           SizedBox(width: 20),
